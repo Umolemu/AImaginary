a0f64c518a00fea66f1d678fb245d811d644e987
Umolemu <lambvries@gmail.com>
2024-06-20 12:36:41 +0200

    Fix mobile lag 1

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+//Function for mobile navigation links to show and hide
+function handleMobileNavLinks() {
+    let mobileNavLinks = document.querySelector('.mobile-links-container');
+    mobileNavLinks.classList.toggle('show');
+}
